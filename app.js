3db269d969ddda26d17e27b347815c02cf39cace
Vito Perez <vitoperez117@gmail.com>
2024-07-11 00:12:27 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const url = "https://public.tableau.com/views/USLivestockImports/TotalperYear?:l
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 1000,
-    width: 1200,
+    height: 800,
+    width: 1000,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
